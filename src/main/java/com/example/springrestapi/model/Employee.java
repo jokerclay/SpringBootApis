a706836d3cb456c156dc06a6632d8998712b0617
jokerclay <2079209547@qq.com>
2022-07-01 01:29:07 +0800

    added entity

--- src/main/java/com/example/springrestapi/model/Employee.java
+++ src/main/java/com/example/springrestapi/model/Employee.java
@@ -6,15 +6,26 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import javax.persistence.*;
+
 @Getter
 @Setter
 @ToString
+//@Entity(name = "tbl_employee")
+@Entity
+@Table(name = "tbl_employee")
 public class Employee {
     // ===========================
     //  @JsonProperty 使字段有其他名字（alias)
     // ===========================
 
-//    @JsonProperty("full_name")
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO) // 对应 数据库中的 自增
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    //    @JsonProperty("full_name")
+    @Column(name = "name")
     private String name;
 
     // ===========================
@@ -23,12 +34,16 @@ public class Employee {
     // ===========================
 
 //    @JsonIgnore
+    @Column(name = "age")
     private Long age;
 
+    @Column(name = "location")
     private String location;
 
+    @Column(name = "email")
     private String email;
 
+    @Column(name = "department")
     private String department;
 
 }
